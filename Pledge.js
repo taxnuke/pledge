e99cca071f52fdaa0ddd07534a90372daa41a85b
Semyon Fomin <taxnuke@gmail.com>
2018-10-27 00:27:10 +0300

    separate resolve and reject handlers, implement catch and chaining

--- Pledge.js
+++ Pledge.js
@@ -1,9 +1,13 @@
 function Pledge(executor) {
-  this._handlers = []
+  this._resolveHandlers = []
+  this._rejectHandlers = []
+  this._err = undefined
   this._value = undefined
   this._state = Pledge.prototype.states.pending
   this._nextPledge = undefined
-  executor(this._resolve.bind(this))
+  this._prevPledge = undefined
+
+  executor(this._resolve.bind(this), this._reject.bind(this))
 }
 
 Pledge.prototype.states = Object.freeze({
@@ -12,8 +16,8 @@ Pledge.prototype.states = Object.freeze({
   rejected: Symbol('rejected')
 })
 
-Pledge.prototype._executeHandlers = function () {
-  this._handlers.forEach(handler => {
+Pledge.prototype._runResolveHandlers = function () {
+  this._resolveHandlers.forEach(handler => {
     const rv = handler(this._value)
 
     if (rv && rv instanceof Pledge) {
@@ -26,19 +30,45 @@ Pledge.prototype._executeHandlers = function () {
   })
 }
 
+Pledge.prototype._runRejectHandlers = function () {
+  this._rejectHandlers.forEach(handler => {
+    const rv = handler(this._err)
+
+    if (rv && rv instanceof Pledge) {
+      rv.then(res => {
+        this._nextPledge._reject(res)
+      })
+    } else {
+      this._nextPledge._reject(rv)
+    }
+  })
+}
+
 Pledge.prototype._resolve = function (val) {
   if (this._state === Pledge.prototype.states.pending) {
     this._value = val
     this._state = Pledge.prototype.states.resolved
-    this._executeHandlers();
+    this._runResolveHandlers();
   }
 }
 
-Pledge.prototype.then = function (handler) {
-  this._handlers.push(handler)
+Pledge.prototype._reject = function (err) {
+  if (this._state === Pledge.prototype.states.pending) {
+    this._err = err
+    this._state = Pledge.prototype.states.rejected
+    this._runRejectHandlers();
 
-  if (this._state === Pledge.prototype.states.resolved) {
-    this._executeHandlers();
+    if (this._nextPledge) {
+      this._nextPledge._reject(err)
+    }
+  }
+}
+
+Pledge.prototype.catch = function (handler) {
+  this._rejectHandlers.push(handler)
+
+  if (this._state === Pledge.prototype.states.rejected) {
+    this._runRejectHandlers();
 
     return this
   }
@@ -46,6 +76,25 @@ Pledge.prototype.then = function (handler) {
   if (!this._nextPledge) {
     this._nextPledge = new Pledge(() => {
     })
+    this._nextPledge._prevPledge = this
+  }
+
+  return this._nextPledge
+}
+
+Pledge.prototype.then = function (handler) {
+  this._resolveHandlers.push(handler)
+
+  if (this._state === Pledge.prototype.states.resolved) {
+    this._runResolveHandlers();
+
+    return this
+  }
+
+  if (!this._nextPledge) {
+    this._nextPledge = new Pledge(() => {
+    })
+    this._nextPledge._prevPledge = this
   }
 
   return this._nextPledge
